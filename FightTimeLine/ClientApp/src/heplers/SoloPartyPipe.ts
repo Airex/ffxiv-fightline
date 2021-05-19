81a9d01ec7b2d77caa8907b6767c66e07b9a30bc
SKachkovsky <airex.s@gmail.com>
2021-05-19 16:52:16 +0300

    fix solo/party filter

--- FightTimeLine/ClientApp/src/heplers/SoloPartyPipe.ts
+++ FightTimeLine/ClientApp/src/heplers/SoloPartyPipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { IAbility, AbilityType } from 'src/core/Models';
 
-type Input ={
+type Input = {
   jobId: string;
   jobName: string;
   ability: IAbility;
@@ -19,8 +19,12 @@ export class SoloPartyPipe implements PipeTransform {
     }
     // filter items array, items which match and return true will be
     // kept, false will be filtered out
-    return items.filter(item => ( !item.ability ||
+    return items.filter(item => (!item.ability ||
       (item.ability.abilityType & AbilityType.PartyDefense) === AbilityType.PartyDefense && filter[1] ||
-      (item.ability.abilityType & AbilityType.SelfDefense) === AbilityType.SelfDefense && filter[0]));
+      (item.ability.abilityType & AbilityType.PartyShield) === AbilityType.PartyShield && filter[1] ||
+      (item.ability.abilityType & AbilityType.SelfDefense) === AbilityType.SelfDefense && filter[0] ||      
+      (item.ability.abilityType & AbilityType.SelfShield) === AbilityType.SelfShield && filter[0] ||
+      (item.ability.abilityType & AbilityType.TargetDefense) === AbilityType.TargetDefense && filter[0]
+    ));
   }
 }
