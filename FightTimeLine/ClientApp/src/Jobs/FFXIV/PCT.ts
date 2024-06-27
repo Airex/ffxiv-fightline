e31d5e119f0d321956c60cc180cf1d59fc1117fa
SKachkovsky <airex.s@gmail.com>
2024-06-27 17:36:23 +0300

    pct vpr stubs

---
+++ FightTimeLine/ClientApp/src/Jobs/FFXIV/PCT.ts
@@ -0,0 +1,22 @@
+import { IJobTemplate, Role } from "src/core/Models";
+
+export const PCT: IJobTemplate = {
+  translation: {
+    en: "PCT",
+    de: "PCT",
+    ja: "PCT",
+    fr: "PCT",
+    cn: "PCT",
+  },
+
+  fullNameTranslation: {
+    en:"Pictomancer",
+    de:"Maler",
+    ja:"Pictomancer",
+    fr:"Pictomancer",
+    cn:"Pictomancer",
+  },
+  role: Role.Caster,
+  abilities:[],
+  traits: [],
+}
---
+++ FightTimeLine/ClientApp/src/Jobs/FFXIV/VPR.ts
@@ -0,0 +1,22 @@
+import { IJobTemplate, Role } from "src/core/Models";
+
+export const VPR: IJobTemplate = {
+  translation: {
+    de: "VPR",
+    ja: "VPR",
+    en: "VPR",
+    fr: "VPR",
+    cn: "VPR",
+  },
+
+  fullNameTranslation: {
+    en: "Viper",
+    de: "Viper",
+    ja: "Viper",
+    fr: "Viper",
+    cn: "Viper",
+  },
+  role: Role.Melee,
+  abilities: [],
+  traits: [],
+};
